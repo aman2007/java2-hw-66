8be253e94928345a69d87ee041f115de0524b7ec
Aman Abdulaev <abdulaev25@gmail.com>
2019-05-27 21:28:24 +0600

    first comit

---
+++ MySimpleService/src/main/java/com/attractor/mySimpleService/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.attractor.mySimpleService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	   @RequestMapping(value = "/")
+
+	   public String home() {
+
+	      return "Eureka Client application";
+
+	   }
+
+}
